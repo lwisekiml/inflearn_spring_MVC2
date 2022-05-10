521b8183fb93961e5a8acb4daabee56959bbfeb5
wisekim <lwisekiml@naver.com>
2022-05-10 20:39:27 +0900

    [타임리프-기본기능] 자바스크립트 인라인

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -149,4 +149,13 @@ public class BasicController {
         addUsers(model);
         return "basic/block";
     }
+
+    // [타임리프-기본기능] 자바스크립트 인라인
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
 }
