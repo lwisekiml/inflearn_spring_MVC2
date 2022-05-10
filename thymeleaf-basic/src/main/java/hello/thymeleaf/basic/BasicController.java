fcfacba483241468a498c95526341df8cdfa50dd
wisekim <lwisekiml@naver.com>
2022-05-10 10:23:38 +0900

    [타임리프-기본기능] 반복

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -111,4 +111,20 @@ public class BasicController {
     public String attribute() {
         return "basic/attribute";
     }
+
+    // [타임리프-기본기능] 반복
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
+    }
 }
