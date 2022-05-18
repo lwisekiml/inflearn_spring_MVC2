6a45d5605bbd428e7482ad2259f4513b572faed5
wisekim <lwisekiml@naver.com>
2022-05-18 19:20:16 +0900

    [메시지, 국제화] 스프링 메시지 소스 사용

--- message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,9 +2,9 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
+//import org.springframework.context.MessageSource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
